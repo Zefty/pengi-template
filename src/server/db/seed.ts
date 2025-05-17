e1db6237cc75891687de4430c71d76ce3e7d8421
Jaime Wu <jaime.wu011@gmail.com>
2025-05-18 11:11:19 +1200

    feat: add gha to bootstrap project + add missing db seed (#1)

---
+++ src/server/db/seed.ts
@@ -0,0 +1,19 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+import { env } from "~/env";
+import { reset, seed } from "drizzle-seed";
+import * as schema from "./schema";
+
+console.log(env.DATABASE_URL);
+const conn = postgres(env.DATABASE_URL);
+const db = drizzle(conn);
+
+const userIds = (await db.select().from(schema.user)).map((user) => user.id);
+console.log(userIds);
+
+await reset(db, { posts: schema.posts });
+await seed(db, { posts: schema.posts }, { count: 100 });
+
+console.log("Done seeding!");
+await conn.end();
